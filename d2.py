90b40e40003cdbc734618b4f754ba17dcb8cc080
zach <zach@nixos.btw>
2025-12-20 16:57:59 -0700

    finished day 2 kddfjkdjf

--- d2.py
+++ d2.py
@@ -1,5 +1,5 @@
-## 
 def process_range(start: int, end: int):
+    result_set: set[int] = set()
     print("-----------")
     print(f'Start: {int(start)}, End: {int(end)}')
 
@@ -7,48 +7,46 @@ def process_range(start: int, end: int):
     end_length = len(str(end))
     size_dif = end_length - start_length
     print(f'Size Difference: {size_dif}')
-    print(f'Numeric Difference: {end - start}')
 
-    new_start = get_first_half(start)
-    new_end = get_first_half(end)
+    for i in range(start, end + 1):
+        if check_series(i):
+            result_set.add(i)
+    return result_set
+            
+def check_series(num: int):
+    first_half = split_num(num)
 
-    print(f'New start: {new_start}')
-    print(f'New end: {new_end}')
+    num_length = len(str(num))
+    if num_length < 2:
+        return False
+    for i in range(1, len(first_half) + 1):
+        substr = first_half[:i]
+        if repeat(substr, num_length) == num:
+            return True
+    return False
 
-
+def repeat(s: str, length: int):
+    n = s
+    while len(n) < length:
+        n += s
+    return int(n)
     
-    
-def is_odd_length(num: int):
-    return len(str(num)) % 2 != 0
-
-# convert to string
 # get first half of string, taking larger portion if odd length e.g. "12345" -> "123"
-def get_first_half(s: int) -> int:
+def split_num(s: int) -> str:
     str_s = str(s)
     n = len(str_s)
-
     mid = (n + 1) // 2
-    return int(str_s[0:mid])
-
-def halfsize(num: int):
-    num_to_str = str(num)
-    length = len(num_to_str)
-    print(f'Half the size of {num} is {num_to_str[0:int(length/2)]}')
-
-def test():
-    # process_range(1, 22)
-    halfsize(1)
-    halfsize(12)
-    halfsize(123)
-    halfsize(1234)
-    halfsize(12345)
+    return  str_s[:mid]
 
 def execute(content: str):
     ranges = content.split(",")
+    total_sum = 0
     for r in ranges:
         (start, end) = map(int, r.split("-"))
-        process_range(start, end)
+        result_set = process_range(start, end)
+        total_sum += sum(result_set)
 
+    print(f'Total sum is: {total_sum}')
 
 def main():
     with open("inputs/d2.txt", "r") as file:
@@ -56,6 +54,45 @@ def main():
     # test()
     execute(content)
 
+def test():
+    x = 121212
+    print(f'{x} is a series: {check_series(x)}')
+
+    x = 123123
+    print(f'{x} is a series: {check_series(x)}')
+
+    x = 121212
+    print(f'{x} is a series: {check_series(x)}')
+
+    x = 111111
+    print(f'{x} is a series: {check_series(x)}')
+
+    x = 111
+    print(f'{x} is a series: {check_series(x)}')
+
+    x = 123
+    print(f'{x} is a series: {check_series(x)}')
+
+    x = 12312
+    print(f'{x} is a series: {check_series(x)}')
+
+    x = 1231231
+    print(f'{x} is a series: {check_series(x)}')
+
+    print(process_range(1, 25))
+
+    print(process_range(11, 22))
+    print(process_range(95, 115))
+    print(process_range(998, 1012))
+    print(process_range(1188511880, 1188511890))
+    print(process_range(222220, 222224))
+    print(process_range(1698522, 1698528))
+    print(process_range(446443, 446449))
+    print(process_range(38593856, 38593862))
+    print(process_range(565653, 565659))
+    print(process_range(824824821, 824824827))
+    print(process_range(2121212118, 2121212124))
 
 if __name__ == "__main__":
     main()
+
