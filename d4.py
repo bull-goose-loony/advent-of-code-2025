13c75a3ff41aaa37f8fc5bd3af3a8d3a71d5b67d
zach <zach@nixos.btw>
2025-12-30 15:08:19 -0700

    cleanup

--- d4.py
+++ d4.py
@@ -1,4 +1,4 @@
-def execute(grid: list[list[str]]) -> list[tuple[int, int]]:
+def get_removable(grid: list[list[str]]) -> list[tuple[int, int]]:
     to_remove: list[tuple[int, int]] = []
     for r, row in enumerate(grid):
         for c, cell in enumerate(row):
@@ -6,22 +6,6 @@ def execute(grid: list[list[str]]) -> list[tuple[int, int]]:
                 if can_remove(grid, r, c):
                     to_remove.append((r, c))
     return to_remove
-
-
-def process_grid(grid: list[list[str]]):
-    changes: list[tuple[int, int]] = []
-    rolls_removed = 0
-
-    while True:
-        changes = execute(grid)
-        count_to_remove = len(changes) 
-
-        if count_to_remove == 0:
-            break
-        rolls_removed += count_to_remove
-        grid = remove_rolls(grid, changes)
-
-    return rolls_removed
 
 def remove_rolls(grid: list[list[str]], coordinates: list[tuple[int, int]]):
     for x, y in coordinates:
@@ -36,57 +20,35 @@ def can_remove(grid: list[list[str]], r: int, c:int):
 def get_roll_count(grid: list[list[str]], r: int, c:int) -> int:
     row_len = len(grid)
     col_height = len(grid[0])
-
-    # print(f"Iteration r = {r}, c = {c}")
-
     roll_count = 0
+
     for sr in range(r-1, r+2):
         if sr < 0:
             continue
         for sc in range(c-1, c+2):
-            # print(f'sr = {sr}, sc = {sc}')
-            if sc < 0:
-                # print('0!')
-                continue
-            if sr > row_len - 1:
-                # print('1!')
-                continue
-            if sc > col_height - 1:
-                # print('2!')
+            if sc < 0 or (sr > row_len -1) or (sc > col_height - 1):
                 continue
             if (sr == r) and (sc == c):
-                # print('3!')
                 continue
-            # print(f"grid[sr][sc] = {grid[sr][sc]}")
             if grid[sr][sc] == '@':
                 roll_count += 1
-
-    print(f"Roll count = {roll_count}")
     return roll_count
 
-def test():
-    print("hi")
-    test_content = """..@@.@@@@.
-@@@.@.@.@@
-@@@@@.@.@@
-@.@@@@..@.
-@@.@@@@.@@
-.@@@@@@@.@
-.@.@.@.@@@
-@.@@@.@@@@
-.@@@@@@@@.
-@.@.@@@.@."""
+def process_grid(grid: list[list[str]]):
+    changes: list[tuple[int, int]] = []
+    rolls_removed = 0
 
-    grid: list[list[str]] = [list(line) for line in test_content.splitlines()]
-    print(grid)
-    sum = execute(grid)
-    print(sum)
+    while True:
+        changes = get_removable(grid)
+        count_to_remove = len(changes) 
 
-def lines_to_grid(lines):
-    grid: list[list[str]] = []
-    for line in lines:
-        grid.append(list(line.strip()))
-    return grid
+        if count_to_remove == 0:
+            break
+
+        rolls_removed += count_to_remove
+        grid = remove_rolls(grid, changes)
+
+    return rolls_removed
 
 def main():
     grid: list[list[str]] = []
@@ -95,7 +57,6 @@ def main():
         for row in rows:
             grid.append(list(row.strip()))
     print(process_grid(grid))
-
 
 if __name__ == "__main__":
     main()
